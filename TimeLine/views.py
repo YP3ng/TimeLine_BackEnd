047e40be4f9d70f2ea45a05fa11783fc4e470c11
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-03 18:45:49 +1100

    views to urls

--- TimeLine/views.py
+++ TimeLine/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Take a request and return a respond
+def say_hi(request):
+    return HttpResponse('Hello World!')

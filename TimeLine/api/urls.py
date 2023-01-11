a6a2b4ab722b7bd9bd28748f95fcc4b864ef7a34
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-11 14:51:30 +1100

    api with input

--- TimeLine/api/urls.py
+++ TimeLine/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('rest', views.getRestRoute),
+    path('rest/<str:input>', views.getNum),
 ]
--- TimeLine/api/views.py
+++ TimeLine/api/views.py
@@ -1,7 +1,22 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def getRoutes(request):
     routes = [
         'GET /api',
     ]
     return JsonResponse(routes, safe = False)
+
+@api_view(['GET'])
+def getRestRoute(request):
+    routes = [
+        'GET /api',
+        'GET /api/rest',
+        'GET /api/rest/:Number'
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getNum(request, input):
+    return Response(input)

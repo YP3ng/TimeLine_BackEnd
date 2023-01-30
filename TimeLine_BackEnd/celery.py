7c5bc3a83b9f4853d935b6cd4c11ee7b9975d169
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-30 21:40:46 +1100

    backend data

--- TimeLine_BackEnd/celery.py
+++ TimeLine_BackEnd/celery.py
@@ -5,7 +5,15 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TimeLine_BackEnd.settings')
 
-app = Celery('TimeLine_BackEnd')
+app = Celery(
+    'TimeLine_BackEnd',
+    broker='amqp://',
+    backend='rpc://'
+)
+
+app.conf.update(
+    result_expires=3600,
+)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.

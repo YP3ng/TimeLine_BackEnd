f36c20b88057e4ac5e18517b567bf6e0046b05a4
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-30 19:00:37 +1100

    restFramework

--- task1/urls.py
+++ task1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:input>', views.my_view),
+    path('', views.my_view),
 
 ]
--- task1/views.py
+++ task1/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from task1.task import calculate
 
+@api_view(['GET'])
+def my_view(request):
+    # apply_async
+    res = calculate.apply_async(
+        [1], 
+        #queue='test', 
+        countdown=5,
+    )
+    print("id: ", res.id)
+    print("state: ", res.state)
 
-def my_view(request, input):
-    res = calculate.delay(input)
 
+    drespond = [
+        "apply_async",
+        "delay",
+    ]
+
+    #res = calculate.delay(2)
     # res is an instance of asyncResult, need to convert
     # However, celery works
-    return Response(res)
+    return Response(drespond)

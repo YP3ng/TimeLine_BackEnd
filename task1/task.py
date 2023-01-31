f0a6b063ada8223b5173945ae376c87e04a7ac01
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-31 17:29:37 +1100

    change state

--- task1/task.py
+++ task1/task.py
@@ -1,11 +1,23 @@
-from celery import shared_task
+from celery import shared_task, current_task
 from time import sleep
 from celery.utils.log import get_task_logger
+
 
 logger = get_task_logger('test')
 
 @shared_task
-def calculate(input):
-    sleep(20)
-    logger.info("Awake!!!!!")
+def add(input):
+    sleep(5)
+    logger.info("add 100")
+    current_task.update_state(
+        state= "CUSTOM", 
+        meta={'input': input, 'result': input+100}
+    )
     return input + 100
+
+
+@shared_task
+def multiply(input):
+    sleep(5)
+    logger.info("multiply by 10")
+    return input * 10

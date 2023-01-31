adab840376b723116bfc8ad8c09c9e8de85ed70a
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-01-31 17:29:26 +1100

    chage workflow

--- task1/views.py
+++ task1/views.py
@@ -1,29 +1,44 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from task1.task import calculate
+from task1.task import add, multiply
 
 @api_view(['GET'])
-def my_view(request):
+def my_view(request, input):
+    lst = [input]
     # apply_async
-    res = calculate.apply_async(
-        [1], 
+    res1 = add.apply_async(
+        lst, 
         #queue='test', 
         #countdown=3,
     )
     print("--------------------We still work while sleeping--------------------")
-
     respond = [
         {
-            'id': res.id,
-            'state_pre': res.state,
-            'result': res.get(),
-            'state_after': res.state,
-            'successful_after': res.successful(),
-            'metadata': res.info,
+            'id1': res1.id,
+            'state_pre': res1.state,
+            'result': res1.get(),
+            'state_after': res1.state,
+            'successful_after': res1.successful(),
+            'metadata': res1.info,
 
         },
     ]
+    res2 = multiply.apply_async(
+        [res1.get()],
+    )
+
+    print("-------------------------After second task--------------------------")
+    
+    respond.append({
+            'id2': res2.id,
+            'state_pre': res2.state,
+            'result': res2.get(),
+            'state_after': res2.state,
+            'successful_after': res2.successful(),
+            'metadata': res2.info,
+        })
+    
 
     print("------------------------------Job done------------------------------")
     return Response(respond)

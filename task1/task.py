bb3c9bc2a3ed4ba22cdd305773b2572cdf14a1b6
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-02-01 18:35:49 +1100

    Spot issue

--- task1/task.py
+++ task1/task.py
@@ -1,22 +1,31 @@
 from celery import shared_task
+from celery.result import AsyncResult
 from time import sleep
 from celery.utils.log import get_task_logger
 
 
-logger = get_task_logger('test')
+logger = get_task_logger(__name__)
 
-@shared_task(bind=True)
+@shared_task(bind=True, name='tasks.add')
 def add(self, input):
     sleep(5) # Simulate expensive operations
     logger.info("add 100")
 
-    # TODO:Update_state not working
-    # Possible issue: RPC backend can't retrieve data twice
-    # Possible solution: Change backend structure to redis
+    # Info about current task
+    #logger.info(self.request)
+
+    # FIXME:Update_state not working
+    # Issue: State gets override by built-in state, 
+    # and RPC backend doesn't store the states in-progress
+    # Solution: Select other backend, maybe Redis or DB
+    logger.info(self.AsyncResult(self.request.id).state)
+
     self.update_state(
         state= "CUSTOM", 
         meta={'input': input, 'result': input+100}
     )
+
+    logger.info(self.AsyncResult(self.request.id).state) # PENDING
     return input + 100
 
 
